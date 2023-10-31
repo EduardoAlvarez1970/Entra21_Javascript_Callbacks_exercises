6ede31b9373a10e7780ab94b03cf15d03a68b13f
Eduardo Alvarez <opticoedu@gmail.com>
2023-10-31 13:49:26 -0300

    added ex callback1 sem resolver

---
+++ callbacks1.js
@@ -0,0 +1,8 @@
+/*Crie uma função que recebe um callback como argumento:
+Dentro dela crie uma variável nome para receber um nome via prompt e em seguida
+execute o callback.
+
+Realize 3 chamadas para essa função passando callbacks diferentes em cada uma, por
+exemplo:
+1- Passar uma função que exibe um alerta com `Olá ${nome}`
+2- Passar uma função que exibe um alerta com `Feliz aniversário ${nome}`*/
