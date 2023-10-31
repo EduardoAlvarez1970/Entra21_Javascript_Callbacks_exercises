70c8362f451ee7bee0121b117a2d07864034ded5
Eduardo Alvarez <opticoedu@gmail.com>
2023-10-31 14:39:00 -0300

    addes promises and async await exercises sem resolver

---
+++ promises_async_await.js
@@ -0,0 +1,21 @@
+
+
+/*Use o fetch para buscar a lista de usuários (https://jsonplaceholder.typicode.com/users)
+e, em seguida, use o método map para armazenar os nomes dos usuários em um Array.*/
+
+
+
+
+/*Utilize o fetch para buscar uma lista de tarefas na API
+https://jsonplaceholder.typicode.com/todos
+Após isso, utilize o método filter para encontrar as tarefas incompletas. Em seguida, use
+o método length para contar quantas tarefas estão incompletas.*/
+
+
+
+
+
+/*Busque a lista de álbuns de fotos de um usuário na API
+https://jsonplaceholder.typicode.com/albums
+Use o método filter para encontrar os álbuns de um usuário de sua escolha pelo userId e
+depois map para exibir o título de cada álbum.*/
